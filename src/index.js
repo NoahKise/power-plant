88d20aaf0f1f6eac3d5c983924a14cad6413c10f
noahkise <noah@kisefamily.com>
2023-11-28 11:56:17 -0800

    add styling and remove excess code
    Co-authored-by: Kim Robinson <kimmykokonut@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const changeState = (prop) => {
         });
     };
 };
-
 const storeState = () => {
     const plantStates = {};
     return (plantId) => {
@@ -26,61 +25,11 @@ const storeState = () => {
     };
 };
 const stateControl = storeState();
-// call stateControl() to return the current state
-
-// const fedPlant = stateControl(blueFood); //soil + 5 *don't do blueFood()
-// const fedPlantAgain = stateControl(greenFood); //soil + 10
-
-//const feed = changeState("soil")(1);
 const blueFood = changeState("soil")(5);
-//const greenFood = changeState("soil")(10);
-// const yuckyFood = changeState("soil")(-5);
-
 const hydrate = changeState("water")(1);
-// const superWater = changeState("water")(5);
-
 const giveLight = changeState("light")(3);
-const plant1 = stateControl("plant1");
-const plant2 = stateControl("plant2");
 
 window.onload = function () {
-    document.getElementById('feed1').onclick = function () {
-        const newState = plant1(blueFood);
-        document.getElementById('soil-value1').innerText = `Soil: ${newState.soil}`;
-    };
-    document.getElementById('water1').onclick = function () {
-        const newState = plant1(hydrate);
-        document.getElementById('water-value1').innerText = `Water: ${newState.water}`;
-    };
-    document.getElementById('light1').onclick = function () {
-        const newState = plant1(giveLight);
-        document.getElementById('light-value1').innerText = `Light: ${newState.light}`;
-    };
-    document.getElementById('show-state1').onclick = function () {
-        const currentState = plant1();
-        document.getElementById('soil-value1').innerText = `Soil: ${currentState.soil}`;
-        document.getElementById('water-value1').innerText = `Water: ${currentState.water}`;
-        document.getElementById('light-value1').innerText = `Light: ${currentState.light}`;
-    };
-
-    document.getElementById('feed2').onclick = function () {
-        const newState = plant2(blueFood);
-        document.getElementById('soil-value2').innerText = `Soil: ${newState.soil}`;
-    };
-    document.getElementById('water2').onclick = function () {
-        const newState = plant2(hydrate);
-        document.getElementById('water-value2').innerText = `Water: ${newState.water}`;
-    };
-    document.getElementById('light2').onclick = function () {
-        const newState = plant2(giveLight);
-        document.getElementById('light-value2').innerText = `Light: ${newState.light}`;
-    };
-    document.getElementById('show-state2').onclick = function () {
-        const currentState = plant2();
-        document.getElementById('soil-value2').innerText = `Soil: ${currentState.soil}`;
-        document.getElementById('water-value2').innerText = `Water: ${currentState.water}`;
-        document.getElementById('light-value2').innerText = `Light: ${currentState.light}`;
-    };
     let counter = 1;
     document.getElementById('newPlant').onclick = function (e) {
         e.preventDefault();
@@ -116,7 +65,7 @@ window.onload = function () {
         newPlantDiv.setAttribute("class", "plantCard");
         newPlantDiv.append(name, soilButton, h3Soil, waterButton, h3Water, lightButton, h3Light);
         body.append(newPlantDiv);
-        
+
         soilButton.onclick = function () {
             const newState = newPlant(blueFood);
             divSoil.innerText = `Soil: ${newState.soil}`;
@@ -129,12 +78,5 @@ window.onload = function () {
             const newState = newPlant(giveLight);
             divLight.innerText = `Light: ${newState.light}`;
         };
-
-
     };
 };
-
-
-
-//feed(5)(plant)
-//blueFood(plant)
